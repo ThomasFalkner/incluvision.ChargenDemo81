4758103997e715af3f50e97967c112578297853b
Thomas Falkner <thomasfalkner@gmail.com>
2021-05-27 09:34:56 +0200

    first commit

---
+++ incluvision.Charge.Dcm/DcmListener.cs
@@ -0,0 +1,89 @@
+﻿using Sagede.OfficeLine.Shared.Customizing;
+using System;
+using System.Linq;
+
+namespace incluvision.Charge.Dcm
+{
+    public class DcmListener : IDcmCallback
+    {
+        /// <summary>
+        /// DCM-Listner
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public bool Entry(IDcmContext context)
+        {
+            var startDcmCall = Sagede.Core.Diagnostics.HighResolutionTimer.Now();
+
+            try
+            {
+                TraceLog.LogVerbose("Dcm-Ausführung gestartet => {0}", context.Listname);
+
+                switch (context.ListId)
+                {
+
+                    #region VKBeleg
+
+
+                    case DcmDefinitionManager.DcmListId.VKBelegProxyAfterTransform:
+                        {
+
+                            var transformCtx = (Sagede.OfficeLine.Wawi.Services.DcmContextBelegProxyAfterTransform)context;
+                            // Alle chargenpflichtigen Artikelpositionen laden
+                            foreach (var pos in transformCtx.Beleg.Positionen.Where(x =>
+                                    x.Positionstyp == Sagede.OfficeLine.Wawi.Tools.Positionstyp.Artikel
+                                    && x.ChargenPflicht >= 2))
+                            {
+                                // Lagerplätze zum Artikel laden
+                                if (pos.LagerplatzLoad.Any())
+                                {
+                                    // Es müssten an dieser Stelle immer Lagerplätze vorhanden sein,
+                                    // andernfalls besteht kein Bestand und es kann auch keine Charge zuordnet werden
+                                    if (pos.Lagerplatz.Any())
+                                    {
+                                        // Hier deinen Code einfügen, um die Charge zu ermitteln, im Beispiel nehme ich den 
+                                        // erstbesten Lagerplatz
+                                        var lp = pos.Lagerplatz.First();
+                                        //Und füge für den von mir im Beispiel verwendeten Artikel 10200030 / Strukturtapete 
+                                        //aus der Max Mustermann Demo-Datenbank und füge die Charge "Samtweiß matt" hinzu
+                                        lp.ChargenCollection.Add(new Sagede.OfficeLine.Wawi.LagerEngine.ChargenEintrag()
+                                        {
+                                            Charge = "Samtweiß matt",
+                                            Menge = pos.Menge,
+                                            //Kein FiFo-Artikel
+                                            BestandsHandle = 0
+                                        });
+                                        // Wichtig! 
+                                        lp.BestandBuchen = true;
+                                        pos.SetIstDirtyLagerbuchung(true);
+                                    }
+
+
+
+                                }
+
+                            }
+                            break;
+                        }
+
+
+                    #endregion VKBeleg
+
+
+                    default:
+                        break;
+                }
+
+                TraceLog.LogVerbose("Dcm-Ausführung beendet => {0}", context.Listname);
+                TraceLog.LogTime("Dcm-Ausführung", startDcmCall);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                TraceLog.LogException("Fehler in Dcm-Ausführung.");
+                TraceLog.LogException(ex);
+                return false;
+            }
+        }
+    }
+}
---
+++ incluvision.Charge.Dcm/TraceLog.cs
@@ -0,0 +1,67 @@
+﻿using Sagede.Core.Logging;
+using System;
+
+namespace incluvision.Charge.Dcm
+{
+    /// <summary>
+    /// Stellt eine, für diese Assembly,
+    /// exclusive Instanz des Loggers zur Verfügung.
+    /// </summary>
+    internal static class TraceLog
+    {
+        private static ILogger _logger;
+        private static readonly object LockObject = new object();
+
+        /// <summary>
+        /// Liefert die Instanz des Loggers.
+        /// </summary>
+        internal static ILogger Logger
+        {
+            get
+            {
+                lock (LockObject)
+                {
+                    return _logger ?? (_logger = LogManager.GetLogger("incluvision.Charge.Dcm", "Logging"));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Loggt Debug-Informationen im TraceLogger_Template1-Manager
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="args"></param>
+        public static void LogVerbose(string message, params object[] args)
+        {
+            //Logger.LogVerboseFormat(message, args);
+            Logger.Verbose(string.Format(message, args));
+        }
+
+        /// <summary>
+        /// Loggt Exceptions im Tracelog-Manager
+        /// </summary>
+        /// <param name="ex"></param>
+        public static void LogException(Exception ex)
+        {
+            Logger.Error(String.Format("{0},{1},{2}", ex.Message, Environment.NewLine, ex.StackTrace));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="args"></param>
+        public static void LogException(string message, params object[] args)
+        {
+            Logger.Error(string.Format(message, args));
+        }
+
+        /// <summary>
+        /// Loggt die Dauer von Aufrufen
+        /// </summary>
+        public static void LogTime(string measurement, long start)
+        {
+            Logger.LogTime(measurement, start);
+        }
+    }
+}
